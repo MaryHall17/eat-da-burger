8365e2b09dfb15e9871cc23845fb63949b4efd09
Mary Hall <mh6uj@students-MBP.home>
2018-02-16 14:15:19 -0500

    user can add burger; shows in terminal, need to render it

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,10 +15,10 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-	burger.createOne([
+	burger.insertOne([
 		"burger_name", "devoured" 
 		], [
-			req.body.burger_name, req.body_devoured
+			req.body.burger_name, req.body.devoured
 			], function(result) {
 				res.json({ id: result.insertId});
 			});
