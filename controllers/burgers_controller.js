2e73feecb3618c71b9261876230da2e9242ad772
Mary Hall <mh6uj@students-MBP.home>
2018-02-16 14:04:59 -0500

    page render and returns raw data packet in terminal

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,11 +5,12 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-	burger.all(function(data) {
+	burger.selectAll(function(data) {
 		var hbsObject = {
 			burgers: data
 		};
 		console.log(hbsObject);
+		res.render("index", hbsObject);
 	});
 });
 
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,39 @@
+$(function() {
+	$(".change-devoured").on("click", function(event) {
+		var id = $(this).data("id");
+		var newDevoured = $(this).data("newdevoured");
+
+		var newDevouredState = {
+			devoured: newDevour
+		};
+
+		$.ajax("/api/burgers/" + id, {
+			type: "PUT",
+			data: newDevouredState
+		}).then(
+		function() {
+			console.log("changed devoured to", newDevour);
+
+			location.reload();
+		});
+	});
+
+	$(".create-form").on("submit", function (event) {
+		event.preventDefault();
+
+		var newBurger = {
+			burger_name: $("#burger").val().trim(),
+			devoured: 0
+		};
+
+		$.ajax("/api/burgers", {
+			type: "POST",
+			data: newBurger
+		}).then(
+			function () {
+				console.log("created new burger");
+				location.reload();
+			});
+	});
+});
+
